b49272ef482c00a70ff0e3941e588dbc0d3c2eba
ezluci <ezluci1@gmail.com>
2020-11-15 19:32:40 +0200

    solving red-white and partially orange-white

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+
+
+
+    bool moving = false;
+    Vector3 lastMouse;
+    Vector2 difMouse;
+    public Transform cameraRef;
+
+    void Update()
+    {
+        if (moving == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (!Physics.Raycast(ray,out hit))
+                {
+                    moving=true;
+                    lastMouse=Input.mousePosition;
+                    //Debug.Log("down_moving");
+                }
+            }
+        }
+        else
+        {
+            if (!(Input.mousePosition == lastMouse))
+            {
+                //Debug.Log("s-a schimbat!!");
+                cameraRef.transform.LookAt(transform);
+                cameraRef.transform.Translate(Vector3.right * Time.deltaTime * 13f);
+                difMouse = lastMouse - Input.mousePosition;
+                /*cameraRef.transform.RotateAround(/*transform.eulerAngles, new Vector3 (1f,1f,1f), 3.5f);*/
+                lastMouse = Input.mousePosition;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                moving=false;
+                //Debug.Log("up_stopmoving");
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
